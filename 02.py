94659745037cd881ecb0042520c38e7032acaa03
Karthik <gangiredlakarthik14@gmail.com>
2025-02-25 21:30:06 +0530

    commit

--- 02.py
+++ 02.py
@@ -7,3 +7,16 @@ for k,v in d1.items():
         marks=v
         name=k
 print(f"{name} scores {marks}")
+#good pairs
+nums = [1,2,3,1,1,3]
+count=0
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        if(nums[i]==nums[j]):
+            count+=1
+#l=[(i,j) for i in range(len(nums)) for j in range(i+1,len(nums)) if(nums[i]==nums[j])] #list comprehension
+#print(len(l))
+print(count)
+#return duplicates from list
+l=[1,2,3,4,2,3,4,5]
+print(list(set(l)))
